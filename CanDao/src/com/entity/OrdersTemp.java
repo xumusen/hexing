cbb9bc5be72c09504190a45fff3441a648f3e15b
xumusen <xumusen@WIN10-XMS>
2019-12-03 23:44:21 +0800

    变更爬虫

---
+++ CanDao/src/com/entity/OrdersTemp.java
@@ -0,0 +1,22 @@
+package com.entity;
+
+import java.util.List;
+
+public class OrdersTemp {
+	String total;
+	List<OrderCollect> rows;
+	public String getTotal() {
+		return total;
+	}
+	public void setTotal(String total) {
+		this.total = total;
+	}
+	public List<OrderCollect> getRows() {
+		return rows;
+	}
+	public void setRows(List<OrderCollect> rows) {
+		this.rows = rows;
+	}
+
+	
+}
